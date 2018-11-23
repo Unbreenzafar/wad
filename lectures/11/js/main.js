8029c1bdbbfc68e7825aaecc046ad0298f0512c3
unbreenzafar <unbreenzafar@ucp.edu.pk>
2018-11-23 16:12:38 +0500

    Merge remote-tracking branch 'remotes/upstream2/master'
    # Conflicts:
    #	lectures/6/css/style.css
    #	lectures/6/index.html

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -2,21 +2,21 @@
 //-----------  STEP - 1 ----------------------
 //============================================
 
-
+/*
 var todos = [];
 function add() {
     var task = document.getElementById("task").value;
     todos.push(task);
     document.getElementById('todos').innerText = todos;
-}
-
-
+}*/
+/*Elements are added with seprated comma and in a single line and does not save previous list when you refresh the page
+*/
 //============================================
 //-----------  STEP - 2 ----------------------
 //============================================
 
-
-/*var todos = [];
+/*
+var todos = [];
 function add() {
     var task = document.getElementById('task').value;
     todos.push(task);
@@ -35,12 +35,13 @@ function show() {
     }
     document.getElementById('todos').appendChild(ul);
 }*/
-
+/* it will show elements in a form of table and repeat  the firstly added elements.
+when ever you tries  to added new elements after editing the first one and does not save the previous tudo list.
+ when you refresh the page*/
 
 //============================================
 //-----------  STEP - 3 ----------------------
 //============================================
-
 
 /*
 function getTodos() {
@@ -83,9 +84,8 @@ function show() {
     }
     document.getElementById('todos').appendChild(ul);
 }
-show();
-*/
-
+show();*/
+/* Adding elements in a table but show delete button that does not work and also save tudo list*/
 
 
 //============================================
@@ -163,8 +163,7 @@ function isDone(e) {
     show();
 }
 show();*/
-
-
+/* Elements are in a table and delete button is working so you can delete any tudo list with it and tudolist remains saved*/
 
 
 
@@ -172,16 +171,135 @@ show();*/
 //-----------  STEP - 5 ----------------------
 //============================================
 
-/*
 
+/*
 CLASS ACTIVITY (HOME WORK) :
 
 1- Enable State of todos by clicking on the text completed, started etc
 HINT: use text-decoration:line-through; property of CSS
 
 2- Enable Editing todos in text field to update text
-
-
-
 */
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i].task + '</li>' +
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>';
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+    }
+}
+
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' +'<p  id="\'+i+\'" onclick="strikethroug(this)">'+ todos[i].task +'</p>' +'</li>' +
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>'+
+            '<button class="btn btn-reset" id1="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> reset </span> </button>';
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    var buttons2 = document.getElementsByClassName('btn-reset');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+        buttons2[i].addEventListener('click',resetval);
+    }
+}
+
+function resetval(){
+    var id = this.getAttribute('id1');
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.splice(id,1);
+    todos.push({task: task, isDone: false});
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+
+function strikethroug(ele)
+{
+    var x = ele.style.color ="red";
+}
+
+
 
